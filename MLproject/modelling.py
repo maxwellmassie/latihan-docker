a1e626329f74df4f99d1020fd86419998c84fd8d
unknown <goldsteinmaxwellmassie@gmail.com>
2025-12-08 04:01:37 +0700

    Revert MLproject/modelling.py to version from e7a4474

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -2,8 +2,8 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import numpy as np
 import os
+import numpy as np
 import warnings
 import sys
 
@@ -11,42 +11,32 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    # Read the dataset
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "train_pca.csv")
     data = pd.read_csv(file_path)
 
+    # The predicted column is "quality" which is a scalar from [3, 9]
     X_train, X_test, y_train, y_test = train_test_split(
-        data.drop("Credit_Score", axis=1),
-        data["Credit_Score"],
-        random_state=42,
-        test_size=0.2
+    data.drop("Credit_Score", axis=1),
+    data["Credit_Score"],
+    random_state=42,
+    test_size=0.2
     )
-
-    input_example = X_train.iloc[:5]
-
+    input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
 
-    # ------------ FIX: gunakan active_run() ------------
-    run = mlflow.active_run()
-    if run is None:
-        run = mlflow.start_run()
-    # ---------------------------------------------------
+        predicted_qualities = model.predict(X_test)
 
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    model.fit(X_train, y_train)
-
-    predicted_qualities = model.predict(X_test)
-
-    mlflow.sklearn.log_model(
+        mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
         input_example=input_example
-    )
-
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
-
-    # jika kita yang memulai run manual, tutup
-    if run and run.info and run.info.run_id:
-        mlflow.end_run()
+        )
+        model.fit(X_train, y_train)
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
